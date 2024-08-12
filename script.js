fe460ec56060c43918475256a5ee3e241635536f
bnirnay <76667341+bnirnay@users.noreply.github.com>
2024-08-12 19:41:29 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("goDark").addEventListener("click", function() {
-        document.body.classList.toggle("dark-mode");
-    });
-});
